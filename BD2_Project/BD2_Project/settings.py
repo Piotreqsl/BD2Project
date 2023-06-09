847107e326425eea03d7d3f66d5c1af63e4eb6c0
Piotreqsl <piotreksl2002@go2.pl>
2023-06-09 10:44:09 +0200

    Added some readme quereis and logging feature

--- BD2_Project/BD2_Project/settings.py
+++ BD2_Project/BD2_Project/settings.py
@@ -72,6 +72,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'BD2_Project.wsgi.application'
 
 
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
